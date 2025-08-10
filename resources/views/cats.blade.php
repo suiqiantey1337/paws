2f3e3bbbd24f2ca5e22ef63aee40be5e00c5ff63
suiqiantey1337 <teysuiqian@gmail.com>
2025-08-10 20:31:55 +0800

    update

--- resources/views/cats.blade.php
+++ resources/views/cats.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Paws & Preferences</title>
+    <title>Paws</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://hammerjs.github.io/dist/hammer.min.js"></script>
