b08801a3bf2a030a0cdc234cb97e8c0aa1d080d3
suiqiantey1337 <teysuiqian@gmail.com>
2025-08-11 11:55:36 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('cats');
+    return view('index');
 });
 
 // Route::get('/dashboard', function () {
